0adda4ec116bbe0a7856b75107ad8f3f679a865a
vladimir-r <rudenko@milax.ru>
2016-05-04 19:47:33 +1100

    Update installer

--- src/Milax/Mconsole/FileArchive/Installer.php
+++ src/Milax/Mconsole/FileArchive/Installer.php
@@ -5,14 +5,17 @@ namespace Milax\Mconsole\FileArchive;
 use Milax\Mconsole\Contracts\Modules\ModuleInstaller;
 
 class Installer implements ModuleInstaller
-{
+{    
     public static function install()
     {
-        // ..
+        //
     }
     
     public static function uninstall()
     {
-        // ..
+        $repository = new \Milax\Mconsole\FileArchive\FileArchiveRepository(\Milax\Mconsole\FileArchive\Models\FileArchive::class);
+        foreach ($repository->get() as $instance) {
+            $instance->delete();
+        }
     }
 }
